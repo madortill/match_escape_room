2238816006384a718e2b75189a26321d5e51f321
madortill <mador.till@gmail.com>
2023-01-10 10:39:02 +0200

    Add files via upload

---
+++ info.js
@@ -0,0 +1,65 @@
+const NUM_OF_LEADERS = 15;
+const CODE = 2;
+// reaction function
+const NUM_OF_EXPLOSION_STAGES = 6;
+const arrLeaders = [{
+    "name": "מושג 1",
+    "act": "מושג 1"
+  },
+  {
+    "name": "ggghgggg hhg",
+    "act": "2"
+  },
+  {
+    "name": "3",
+    "act": "3"
+  },
+  {
+    "name": "4",
+    "act": "4"
+  },
+  {
+    "name": "5",
+    "act": "5"
+  },
+  {
+    "name": "6",
+    "act": "6"
+  },
+  {
+    "name": "7",
+    "act": '7'
+  },
+  {
+    "name": "8",
+    "act": '8'
+  },
+  {
+    "name": "9",
+    "act": '9'
+  },
+  {
+    "name": "10",
+    "act": '10'
+  },
+  {
+    "name": "11",
+    "act": "11"
+  },
+  {
+    "name": "12",
+    "act": '12'
+  },
+  {
+    "name": "13",
+    "act": `13`
+  },
+  {
+    "name": "14",
+    "act": "14"
+  },
+  {
+    "name": "15",
+    "act": "15"
+  }
+];
